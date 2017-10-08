9fd42b89a33cd46d5d9448690bc9045701c95bc2
Felipe Montoya R <fmontoya04@gmail.com>
2017-10-07 18:50:30 -0700

    Initial Commit

---
+++ EmailAppProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmailAppProject
+//
+//  Created by Felipe Montoya on 10/7/17.
+//  Copyright © 2017 Felipe Montoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
